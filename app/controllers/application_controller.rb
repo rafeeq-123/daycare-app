ba20bc108d9b557f4e8d84e6356a5a4aef0de48d
rafeeq-123 <rafeeq-123@hotmail.com>
2016-09-22 18:40:19 -0700

    adding controller funtionality and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    set :session_secret, 'the_house'
 
   end
 
-  get '/' do
-    erb :index
-  end
+  # get '/' do
+  #   erb :index
+  # end
 
 end
--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,2 +1,22 @@
 class ParentsController < ApplicationController
+
+  get '/' do
+    @parents = Parent.all
+    erb :'parents/index'
+  end
+
+  get '/parents/new' do
+    @parent = Parent.new
+    if @parent.valid?
+      redirect :'parent/show'
+    else
+      redirect :"parent/index"
+    end
+  end
+
+  get '/parents/:id' do
+    @parent = Parent.find(params[:id])
+    erb :'parent/show'
+  end
+
 end
