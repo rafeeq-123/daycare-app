0e5d8e3091a5578a12f5ba7259dcde1ed8b90af4
rafeeq-123 <rafeeq-123@hotmail.com>
2016-09-23 19:08:53 -0700

    having problems displaying pages using TDD to understand the problem

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -5,26 +5,27 @@ class ParentsController < ApplicationController
     erb :'parents/index'
   end
 
+  post '/parents/:id' do
+    @parent = Parent.find(params[:id])
+      erb :'parents/show'
+      puts params.to_s
+  end
+
   get '/parents/new' do
     erb :'parents/new'
   end
 
-  get '/parents/:id' do
-    @parent = Parent.find(params[:id])
-    erb :'parent/show'
-  end
-
   post '/parents' do
-    @parent = Parent.create!(params[:name])
+    @parent = Parent.create(params[:name][:special_requests])
+
    if @parent.valid?
       @parent.save
       flash[:success] = "You have successfully created an account"
-      redirect "/"
+      redirect 'parents/show'
   else
       flash[:error] = "Something went wrong. Please try again!"
-      redirect "new"
+      redirect '/parents/new'
     end
-
   end
 
 
